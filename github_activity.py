1226b430857a13462fc7889834d0c2e35f8aed62
danieldiazde <daniel.diaz.deleon@outlook.com>
2025-09-23 16:35:07 -0600

    Format output

--- github_activity.py
+++ github_activity.py
@@ -2,30 +2,32 @@ import urllib.request
 from urllib.error import URLError, HTTPError
 import json
 import argparse
+from json import JSONDecodeError
 
 parser = argparse.ArgumentParser(
-    description='Input your Github username to fetch your resent activity')
+    description='Input your Github username to fetch your recent activity')
 
 parser.add_argument('username',help='Enter your Github username')
 
-API_url = 'https://api.github.com/users/<username>/events'
-
-def format_data(data: dict): #Each dictionary is an event of actions on Github with "id, "type", "actor", "repo", "created_at", "payload"
+def format_data(data: dict) -> str:
     event_id = data.get('id')
     event_type = data.get('type')
     event_actor = data.get('actor', {}).get('login')
     event_repo = data.get('repo', {}).get('name')
     event_created_at = data.get('created_at')
-    event_payload = data.get('ref_type') 
-    print(f'Event {event_type} on repository {event_repo} at time {event_created_at}')
+    event_payload = data.get('payload', {}).get('ref_type') 
 
+    if event_payload:
+        return f'<{event_id}> {event_type} by {event_actor} on {event_payload} on repository {event_repo} at time {event_created_at}'
+    elif event_payload == None or event_payload == '':
+        return f'<{event_id}> {event_type} by {event_actor} on {event_repo} at time {event_created_at}'
 
 def main():
     args = parser.parse_args()
     username = args.username
-    url = API_url.replace('<username>', username)
+    API_url = 'https://api.github.com/users/{}/events'.format(username)
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(API_url) as response:
             response_string = response.read().decode('utf-8')
             data = json.loads(response_string)
             for event in data:
@@ -33,6 +35,9 @@ def main():
             
     except (URLError, HTTPError) as e:
         print(f'Error {e} while fetching data')
+    
+    except JSONDecodeError as e:
+        print(f'Error {e}, failed to decode jason')
 
 if __name__ == '__main__':
     main()
